4511816c556d8d24e5e6841dd41f09933816b758
Jacek Foremski <jacek.foremski@gmail.com>
2017-05-08 13:01:11 +0200

    Implement the Tweet\Value class

--- eZ/Publish/FieldType/Tweet/Value.php
+++ eZ/Publish/FieldType/Tweet/Value.php
@@ -12,4 +12,38 @@ use eZ\Publish\Core\FieldType\Value as BaseValue;
 
 class Value extends BaseValue
 {
+    /**
+     * Tweet URL on twitter.com.
+     *
+     * @var string
+     */
+    public $url;
+
+    /**
+     * Author's Twitter URL (https://twitter.com/UserName)
+     *
+     * @var string
+     */
+    public $authorUrl;
+
+    /**
+     * The tweet's embed HTML
+     *
+     * @var string
+     */
+    public $contents;
+
+    public function __construct($arg = [])
+    {
+        if (!is_array($arg)) {
+            $arg = ['url' => $arg];
+        }
+
+        parent::__construct($arg);
+    }
+
+    public function __toString()
+    {
+        return (string)$this->url;
+    }
 }
