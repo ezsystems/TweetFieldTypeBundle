516cf4e328ab20afa6f0e822f782f9fccee68272
Jacek Foremski <jacek.foremski@gmail.com>
2017-05-08 12:55:38 +0200

    Structure the bundle

---
+++ eZ/Publish/FieldType/Tweet/Type.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * File containing the Tweet FieldType Type class.
+ *
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+
+namespace EzSystems\TweetFieldTypeBundle\eZ\Publish\FieldType\Tweet;
+
+use eZ\Publish\Core\FieldType\FieldType;
+
+class Type extends FieldType
+{
+}
