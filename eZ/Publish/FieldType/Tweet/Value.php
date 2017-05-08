3e5a5659710b6d3a0d8fcc468aca863655cbf41e
Jacek Foremski <jacek.foremski@gmail.com>
2017-05-08 12:55:38 +0200

    Structure the bundle

---
+++ eZ/Publish/FieldType/Tweet/Value.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * File containing the Tweet FieldType Value class.
+ *
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+
+namespace EzSystems\TweetFieldTypeBundle\eZ\Publish\FieldType\Tweet;
+
+use eZ\Publish\Core\FieldType\Value as BaseValue;
+
+class Value extends BaseValue
+{
+}
