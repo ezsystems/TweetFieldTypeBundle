c4538ccc11b50804f3e1541f702cf7fc2772f8d4
Jacek Foremski <jacek.foremski@gmail.com>
2017-05-16 10:49:54 +0200

    Add field view and field definition view templates

--- DependencyInjection/EzSystemsTweetFieldTypeExtension.php
+++ DependencyInjection/EzSystemsTweetFieldTypeExtension.php
@@ -6,14 +6,22 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
+use Symfony\Component\Yaml\Yaml;
 
 /**
  * This is the class that loads and manages your bundle configuration.
  *
  * @link http://symfony.com/doc/current/cookbook/bundles/extension.html
  */
-class EzSystemsTweetFieldTypeExtension extends Extension
+class EzSystemsTweetFieldTypeExtension extends Extension implements PrependExtensionInterface
 {
+    public function prepend(ContainerBuilder $container)
+    {
+        $config = Yaml::parse(file_get_contents(__DIR__.'/../Resources/config/ez_field_templates.yml'));
+        $container->prependExtensionConfig('ezpublish', $config);
+    }
+
     /**
      * {@inheritdoc}
      */
