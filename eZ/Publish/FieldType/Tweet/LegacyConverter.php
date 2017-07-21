3e88443a6f297c75138ddf10a1a76715c790482f
Jacek Foremski <jacek.foremski@gmail.com>
2017-07-21 11:26:09 +0200

    Add a validation

--- eZ/Publish/FieldType/Tweet/LegacyConverter.php
+++ eZ/Publish/FieldType/Tweet/LegacyConverter.php
@@ -30,12 +30,21 @@ class LegacyConverter implements Converter
 
     public function toStorageFieldDefinition(FieldDefinition $fieldDef, StorageFieldDefinition $storageDef)
     {
-        // @todo implement me
+        $storageDef->dataText1 = json_encode(
+            $fieldDef->fieldTypeConstraints->validators['TweetValueValidator']['authorList']
+        );
     }
 
     public function toFieldDefinition(StorageFieldDefinition $storageDef, FieldDefinition $fieldDef)
     {
-        // @todo implement me
+        $authorList = json_decode($storageDef->dataText1);
+        if (!empty($authorList)) {
+            $fieldDef->fieldTypeConstraints->validators = [
+                'TweetValueValidator' => [
+                    'authorList' => $authorList
+                ],
+            ];
+        }
     }
 
     /**
