7727a59bb4824dcea4bf8dabc81e2871d164ca39
Dawid Parafiński <dawid.parafinski@ez.no>
2018-04-06 11:22:27 +0200

    fixup! Helper for tagging tutorial steps

--- Resources/misc/tag-steps.sh
+++ Resources/misc/tag-steps.sh
@@ -12,15 +12,15 @@ declare -a commits=(
     "Add a validation"
     )
 declare -a tags=(
-    "step1_create_the_bundle"
-    "step2_structure_the_bundle"
-    "step3_implement_the_tweet_value_class"
-    "step4_implement_the_tweet_type_class"
-    "step5_register_the_field_type_as_a_service"
-    "step6_implement_the_legacy_storage_engine_converter"
-    "step7_add_field_view_and_field_definition_view_templates"
-    "step8_add_content_and_edit_views"
-    "step9_add_a_validation"
+    "step1_create_the_bundle_v2"
+    "step2_structure_the_bundle_v2"
+    "step3_implement_the_tweet_value_class_v2"
+    "step4_implement_the_tweet_type_class_v2"
+    "step5_register_the_field_type_as_a_service_v2"
+    "step6_implement_the_legacy_storage_engine_converter_v2"
+    "step7_add_field_view_and_field_definition_view_templates_v2"
+    "step8_add_content_and_edit_views_v2"
+    "step9_add_a_validation_v2"
     )
 
 numberOfCommits=${#commits[@]}
